5cb5ca0d0d884ac997db301c7e51f9179cdaf3af
heldinho <hfpsis25@gmail.com>
2023-11-28 01:12:17 -0300

    add inputs
    Signed-off-by: heldinho <hfpsis25@gmail.com>

--- src/app/pages/clients/clients.component.ts
+++ src/app/pages/clients/clients.component.ts
@@ -1,10 +1,29 @@
 import { Component } from '@angular/core';
 
+interface IValue {
+  name: string;
+  document: string;
+  email: string;
+  celular: string;
+}
+
 @Component({
   selector: 'app-clients',
   templateUrl: './clients.component.html',
-  styleUrls: ['./clients.component.scss']
+  styleUrls: ['./clients.component.scss'],
 })
 export class ClientsComponent {
+  tab: number = 1;
+  values: IValue = {
+    name: '',
+    document: '',
+    email: '',
+    celular: '',
+  };
 
+  constructor() {}
+
+  keyup(event: any, val: string) {
+    this.values.name = val;
+  }
 }
